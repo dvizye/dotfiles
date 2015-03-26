3a6f083708c492e76fff34ce272a885b2165e40e
David Ye <dzy@ubuntu.(none)>
2015-03-25 23:08:12 -0700

    Correct setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 DIR=$( cd "$( dirname "$0" )" && pwd )
-if [ ! -f ~/.gitconfig ]
 then
-    ln -s "$DIR/gitconfig" "~/.gitconfig"
+    ln -s "$DIR/gitconfig" ~/.gitconfig
     ln -s "$DIR/git_template" ~/.git_template
 fi
 if [ ! -f ~/.zshrc ]
 then
-    ln -s "$DIR/zshrc" "~/.zshrc"
-    ln -s "$DIR/zshrc.mine" "~/.zshrc.mine"
+    ln -s "$DIR/zshrc" ~/.zshrc
+    ln -s "$DIR/zshrc.mine" ~/.zshrc.mine
     ln -s "$DIR/zshrc.mac" ~/.zshrc.mac
 fi
 
