07cc78683883536ae9c6d3d973cc5df56c21401c
David Ye <dvizye@gmail.com>
2018-10-12 16:26:59 -0700

    More aliases

--- aliases.sh
+++ aliases.sh
@@ -76,3 +76,26 @@ unamestr=`uname`
 #     alias vim='DYLD_FORCE_FLAT_NAMESPACE=1 mvim -v'
 # fi
 alias tmux='TERM=screen-256color-bce tmux'
+
+# Bazel aliases
+alias bb='bazel build'
+alias br='bazel run'
+alias bt='bazel test'
+alias bba='bazel build --config=away'
+alias bra='bazel run --config=away'
+alias bta='bazel test --config=away'
+alias bt_valgrind='bazel test --run_under=valgrind'
+alias br_valgrind='bazel_run --run_under=valgrind'
+
+# Arc (Phabricator) aliases
+alias aland='arc land --revision'
+
+# Point vim to vim.gnome
+alias vim='vim.gnome'
+
+# Bookmarks
+export AV="$HOME/av"
+
+# Git aliases
+alias gd='git diff'
+alias gs='git status'
--- bashrc
+++ bashrc
@@ -108,3 +108,14 @@ export LC_ALL=en_US.UTF-8
 if [ -f "$DIR/machine_specific/bashrc" ];
   then source "$DIR/machine_specific/bashrc";
 fi
+
+# Turn on colors in ls.
+export LS_OPTIONS='--color=auto'
+eval "$(dircolors -b)"
+alias ls='ls $LS_OPTIONS'
+
+# Show git branch in PS1.
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
