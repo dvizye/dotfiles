16ce9417b99e51011f20bc393f9a47d5493ccbb1
David Ye <dvizye@gmail.com>
2018-12-31 05:49:34 -0500

    Remove zsh

--- aliases.sh
+++ aliases.sh
@@ -1,86 +1,27 @@
+# Set paths to dotfiles dir.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Source machine specific aliases
 if [[ $DIR/machine_specific/aliases.sh ]]; then
   source $DIR/machine_specific/aliases.sh
 fi
 
-if test -n "$ZSH_VERSION"; then
-  PROFILE_SHELL=zsh
-elif test -n "$BASH_VERSION"; then
-  PROFILE_SHELL=bash
-elif test -n "$KSH_VERSION"; then
-  PROFILE_SHELL=ksh
-elif test -n "$FCEDIT"; then
-  PROFILE_SHELL=ksh
-elif test -n "$PS3"; then
-  PROFILE_SHELL=unknown
-else
-  PROFILE_SHELL=sh
-fi
-
-# Detect platform
-PLATFORM="unknown"
-case "$(uname -s)" in
-   Darwin)
-     PLATFORM="mac"
-     ;;
-   Linux)
-     PLATFORM="linux"
-     ;;
-   CYGWIN*|MINGW32*|MSYS*)
-     PLATFORM="windows"
-     ;;
-   *)
-     echo "Error: Could not detect OS"
-     ;;
-esac
-
-# Aliases for ls
+# Aliases for ls.
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
 alias cl=changeDirectory
 function changeDirectory {
     cd $1 ; ls
 }
-
-if [[ $PROFILE_SHELL = "zsh" ]]; then
-    alias rebash='source ~/.zshrc'
-else
-    [[ $PLATFORM = "mac" ]] && alias rebash="source ~/.bash_profile" ||
-    alias rebash="source ~/.bashrc"
-fi
-
-[[ -f ~/.bookmarks ]] && alias book="source ~/.bookmarks"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-[[ -f $DIR/google_aliases.sh ]] && source "$DIR/google_aliases.sh"
-
 alias ..='cl ..'
 
+# Aliases for find.
 alias ff='find . -iname'
-# ssh into GCE instance w/ gpu
-alias gpu='gcloud compute --project "tf-box-217802" ssh --zone "asia-east1-a" "instance-2"'
-
-# Helper functions
-function changeDirectory {
-    cd $1 ; ls
-}
-
 function fd {
     find . -name $1 -type d
 }
 
-# Machine/Application specific
-alias robocode='~/robocode/robocode.sh'
-
-# Mac-specific aliases
-# (this is in addition to the script for mvim in order to get tmux to recognize that vim is running)
-unamestr=`uname`
-# if [[ "$unamestr" == 'Darwin' ]]; then
-#     alias vim='DYLD_FORCE_FLAT_NAMESPACE=1 mvim -v'
-# fi
 alias tmux='TERM=screen-256color-bce tmux'
 
 # Bazel aliases
@@ -94,13 +35,8 @@ alias bt_valgrind='bazel test --run_under=valgrind'
 alias br_valgrind='bazel_run --run_under=valgrind'
 
 # Arc (Phabricator) aliases
-alias aland='arc land --revision'
-
-# Point vim to vim.gnome
-alias vim='vim.gnome'
-
-# Bookmarks
-export AV="$HOME/av"
+alias land='arc land --revision'
+alias lint='arc lint --apply-patches'
 
 # Git aliases
 alias gd='git diff'
